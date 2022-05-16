af9ee541d3dd81200124b901a1e782c5b1504a1d
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-16 10:45:51 -0300

    svelte sintax for click and variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-	export let name;
+  let name = "Rian";
+  let beltColour = "black";
+
+  const handleClick = ()=> {
+    beltColour = 'orange'
+  }
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <p>{beltColour} belt</p>
+
+  <button on:click={handleClick}>update belt color</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
