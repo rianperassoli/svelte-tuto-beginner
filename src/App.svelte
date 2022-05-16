cfa340a4ed9100d5ca9aeee5b87979c1b04b0ba5
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-16 16:59:41 -0300

    Created Modal component and passed parameters and events for the component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,8 @@
 <script>
+  import Modal from "./Modal.svelte";
+
+  let showModal = false;
+
   let people = [
     { name: "yoshi", beltColour: "black", age: 25, id: 1 },
     { name: "mario", beltColour: "orange", age: 45, id: 2 },
@@ -9,18 +13,15 @@
     people = people.filter((person) => person.id !== id);
   };
 
-  let num = 5;
+  const toogleModal = () => {
+    showModal = !showModal;
+  };
 </script>
 
-{#if num > 20}
-  <p>Greater than 20</p>
-{:else if num > 5}
-  <p>Greater than 5</p>
-{:else}
-  <p>Not greater than 5</p>
-{/if}
+<Modal message="Message asd asd" {showModal} on:click={toogleModal}/>
 
 <main>
+  <button on:click={toogleModal}>Open modal</button>
   {#each people as person (person.id)}
     <div>
       <h4>{person.name}</h4>
